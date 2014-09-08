b42fc5733bb7c87dee668187b12e1deab1830f39
Tobias Kortkamp <tobias.kortkamp@gmail.com>
2014-09-09 00:05:02 +0200

    Move CoreNLP/ClearNLP dependencies to separate modules

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :license {:name "GNU Lesser General Public License"
 	    :url "http://www.gnu.org/licenses/lgpl.html"}
 
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [t6/snippets-corenlp "0.1.0-SNAPSHOT"]
+                                  [t6/snippets-clearnlp "0.1.0-SNAPSHOT"]]
 		   :plugins [[lein-midje-doc "0.0.24"]
-			     [lein-midje "3.1.3"]]}}
+			     [lein-midje "3.1.3"]
+                             [lein-modules "0.3.8"]]}}
 
   :documentation {:files {"docs/index"
 			  {:input "test/tobik/snippets/guide.clj"
@@ -15,28 +18,18 @@
 			   :author "Tobias Kortkamp"
 			   :email  "tobias.kortkamp@gmail.com"}}}
 
+  :modules {:inherited
+            {:url "https://github.com/t6/snippets"
+             :scm {:dir ".."}
+             :license {:name "GNU Lesser General Public License"
+                       :url "http://www.gnu.org/licenses/lgpl.html"}}}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
 		 [org.clojure/core.logic "0.8.8"]
 		 [org.clojure/tools.reader "0.8.8"]
 		 [org.clojure/tools.macro "0.1.5"]
 
-		 [t6/graph-transform "0.1.0"]
-
-		 [rhizome "0.2.1"]
-
-		 [com.clearnlp/clearnlp "2.0.2"]
-		 [com.clearnlp/clearnlp-dictionary "1.0"]
-		 [com.clearnlp/clearnlp-general-en-pos "1.1"]
-		 [com.clearnlp/clearnlp-general-en-dep "1.2"]
-		 [com.clearnlp/clearnlp-general-en-srl "1.1"]
-
-		 [org.apache.opennlp/opennlp-tools "1.5.3"]
-		 [edu.washington.cs.knowitall/opennlp-tokenize-models "1.5"]
-		 [edu.washington.cs.knowitall/opennlp-sent-models "1.5"]
-
+                 [rhizome "0.2.1"]
+		 
 		 [camel-snake-kebab "0.2.4"]
-		 [prismatic/plumbing "0.3.3"]
-
-		 [edu.stanford.nlp/stanford-corenlp "3.4.1"]
-		 [edu.stanford.nlp/stanford-corenlp "3.4"
-		  :classifier "models"]])
+		 [prismatic/plumbing "0.3.3"]])
---
+++ snippets-corenlp/project.clj
@@ -0,0 +1,6 @@
+(defproject t6/snippets-corenlp "0.1.0-SNAPSHOT"
+  :plugins [[lein-modules "0.3.8"]]
+  :description "Dependencies for snippets' CoreNLP pipeline"
+  :dependencies [[edu.stanford.nlp/stanford-corenlp "3.4.1"]
+                 [edu.stanford.nlp/stanford-corenlp "3.4"
+                  :classifier "models"]])
